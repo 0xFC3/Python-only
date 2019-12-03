983e64cca3dd992780ba941c1cdf1758d9062786
Leonardo <42663001+FloxiGH@users.noreply.github.com>
2019-12-03 15:22:43 +0100

    Add files via upload

--- Game of Life/cell.py
+++ Game of Life/cell.py
@@ -2,6 +2,7 @@ import pygame
 class Cell():
     def __init__(self, rect, x, y, l=(0,0,0)):
         self.c = l
+        self.cnew = l
         self.rect = rect
         self.posx = x
         self.posy = y
@@ -9,8 +10,9 @@ class Cell():
 
     def iteration(self):
         if self.count < 2 or self.count > 3:
-            self.c = (0, 0, 0)
+            self.cnew = (0, 0, 0)
         elif self.count == 3:
-            self.c = (255, 255, 255)
-
+            self.cnew = (255, 255, 255)
+        else:
+            self.cnew = self.c
         self.count = 0
--- Game of Life/window.py
+++ Game of Life/window.py
@@ -1,6 +1,8 @@
 import pygame
+import random
 from pygame.locals import *
 from cell import Cell
+
 
 def determine_mouseOver(rect, valx, valy):
   if rect.collidepoint(valx, valy):
@@ -8,10 +10,18 @@ def determine_mouseOver(rect, valx, valy):
   else:
     return False
 
-size = 50
+def randomGen(rects):
+  for rect in rects:
+    if random.randint(0,1) == 0:
+      rect.c = (0,0,0)
+    else:
+      rect.c = (255,255,255)
+  return rects
+
+size = 25
 background_colour = (0,0,0)
-(width, height) = (1000, 600)
-(numx, numy) = (int(width / size), int(height / size))
+(width, height) = (1200, 600)
+(numx, numy) = (int((width - 200) / size), int(height / size))
 
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Game of Life')
@@ -20,11 +30,13 @@ clock = pygame.time.Clock()
 
 # drawing rects
 rects = []
-for i in range(numx):
-  rects.append([])
-  for d in range(numy):
-    rects[i].append(Cell(pygame.Rect(i * size, d * size, size, size), i, d))
+for d in range(numy):
+  for i in range(numx):
+    rects.append(Cell(pygame.Rect(i * size, d * size, size, size), i, d))
 
+# GUI elements
+ranGen = pygame.Rect(1050, 200, 100, 50)
+guiBack = pygame.Rect(1000,0,200,600)
 mousex = 0
 mousey = 0
 
@@ -46,32 +58,40 @@ while running:
       if event.key == pygame.K_RIGHT:
         sim = True
 
+  # the simulation
   if sim == True:
-    for row in rects:
-      for rect in row:
-        n = ((rect.posx - 1, rect.posy - 1), (rect.posx - 1, rect.posy), (rect.posx - 1, rect.posy + 1),
-             (rect.posx, rect.posy - 1), (rect.posx, rect.posy + 1), (rect.posx + 1, rect.posy - 1),
-             (rect.posx + 1, rect.posy), (rect.posx + 1, rect.posy + 1))
-        for nrow in rects:
-          for nrect in nrow:
-            for nei in n:
-              if (nrect.posx, nrect.posy) == nei and nrect.c == (255,255,255):
-                rect.count += 1
-            print(rect.count)
-            rect.iteration()
+    for rect in rects:
+      n = ((rect.posx - 1, rect.posy - 1), (rect.posx - 1, rect.posy), (rect.posx - 1, rect.posy + 1),
+           (rect.posx, rect.posy - 1), (rect.posx, rect.posy + 1), (rect.posx + 1, rect.posy - 1),
+           (rect.posx + 1, rect.posy), (rect.posx + 1, rect.posy + 1))
 
-    clock.tick(1000)
+      for i in range((rect.posy - 1) * numx, (rect.posy + 2) * numx):
+        try:
+          for nei in n:
+            if (rects[i].posx, rects[i].posy) == nei and rects[i].c == (255, 255, 255):
+              rect.count += 1
+              continue
+        except: continue
+      rect.iteration()
+    for rect in rects:
+      rect.c = rect.cnew
+    pygame.time.wait(250)
 
-  for row in rects:
-    for rect in row:
-      if determine_mouseOver(rect.rect, mousex, mousey) and mouseClicked == True:
-        rect.c = (255,255,255)
-        pygame.draw.rect(screen, rect.c, rect.rect)
-      elif determine_mouseOver(rect.rect, mousex, mousey) and mouseClicked == False and rect.c !=(255,255,255):
-        pygame.draw.rect(screen, (125, 125, 125), rect.rect)
-      else:
-        pygame.draw.rect(screen, rect.c, rect.rect)
+  # modefying the rects
+  for rect in rects:
+    if determine_mouseOver(rect.rect, mousex, mousey) and mouseClicked == True:
+      rect.c = (255,255,255)
+      pygame.draw.rect(screen, rect.c, rect.rect)
+    elif determine_mouseOver(rect.rect, mousex, mousey) and mouseClicked == False and rect.c !=(255,255,255):
+      pygame.draw.rect(screen, (125, 125, 125), rect.rect)
+    else:
+      pygame.draw.rect(screen, rect.c, rect.rect)
 
+  # GUI elements
+  pygame.draw.rect(screen, (0, 255, 0), guiBack)
+  if determine_mouseOver(ranGen, mousex, mousey) and mouseClicked == True:
+    randomGen(rects)
+  pygame.draw.rect(screen, (255, 0,0), ranGen)
   pygame.display.update()
   clock.tick(50)
 
